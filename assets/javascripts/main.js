eb5ad07ab3daae79404be68e98b82de681ada434
Evan Turner <evturn@gmail.com>
2015-03-01 01:49:33 -0500

    corrections made to cat objects

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,8 +1,8 @@
 $(function() {
 	var cats = new AngryCats([
-		{name: 'Damp Ass Cat' image: 'assets/images/cat1.jp'},
-		{name: 'Spolied Ass Cat' image: 'assets/images/cat2.jp'},
-		{name: 'Anxeity Disorder Cat' image: 'assets/images/cat3.jp'}
+		{name: 'Spolied Ass Cat', image: 'assets/images/cat1.jpg'},
+		{name: 'Damp Ass Cat', image: 'assets/images/cat2.jpg'},
+		{name: 'Anxeity Disorder Cat', image: 'assets/images/cat3.jpg'}
 	]);
 
 	CatApp.start({cats: cats});
