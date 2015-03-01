4a40413430ef2b39577251abfedf88de781416d9
Evan Turner <evturn@gmail.com>
2015-03-01 01:43:42 -0500

    image attribute added to cats array

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,8 +1,8 @@
 $(function() {
 	var cats = new AngryCats([
-		{name: 'Damp Ass Cat'},
-		{name: 'Spolied Ass Cat'},
-		{name: 'Anxeity Disorder Cat'}
+		{name: 'Damp Ass Cat' image: 'assets/images/cat1.jp'},
+		{name: 'Spolied Ass Cat' image: 'assets/images/cat2.jp'},
+		{name: 'Anxeity Disorder Cat' image: 'assets/images/cat3.jp'}
 	]);
 
 	CatApp.start({cats: cats});
